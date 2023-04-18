7359f4a1445415e4dd751594b2a87a330a055b02
Mantas Santockis <mantas.san@gmail.com>
2023-04-18 10:10:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+
+const word = document.querySelector(".input-text")
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".results")
+
+
+btn.addEventListener("click", countVowel)
+
