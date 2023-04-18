4390d9416a4eda6fe90773a3b7b49769ef556596
Mantas Santockis <mantas.san@gmail.com>
2023-04-18 10:20:57 +0300

    finish project

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ const result = document.querySelector(".results")
 
 btn.addEventListener("click", countVowel)
 
+function countVowel() {
+    let vowelCount = 0
+    let wordVal = word.value.toLowerCase()
+
+    for (let i = 0; i < wordVal.length; i++) {
+        const letter = wordVal[i];
+        if (letter.match(/([a,e,o,u,i])/)) {
+            vowelCount++;
+        }
+
+    }
+
+    result.innerHTML = `${word.value.toUpperCase()} has ${vowelCount} vowels`
+}
